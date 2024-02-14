0da289e45a538dcf553e3d71ffa6e9f082bea101
lavkushry <lavkushry@gmail.com>
2024-02-14 22:54:57 +0530

    Added many Pattern

---
+++ Pattern_Programs/Pattern8.java
@@ -0,0 +1,18 @@
+package Pattern_Programs;
+
+public class Pattern8 {
+    public static void main(String[] args) {
+        int n=15;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if( i+j <= (n-1)/2 || j-i>=(n-1)/2 || i-j>=(n-1)/2 || i+j >= n-1+(n-1)/2) {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Pattern_Programs/Pattern9.java
@@ -0,0 +1,35 @@
+/*
+ * * * * * * * * * * * * * * *
+ * * * * * * *   * * * * * * *
+ * * * * * *       * * * * * *
+ * * * * *           * * * * *
+ * * * *               * * * *
+ * * *                   * * *
+ * *                       * *
+ *                           *
+ *                           *
+ *                           *
+ *                           *
+ *                           *
+ *                           *
+ *                           *
+ * * * * * * * * * * * * * * *
+
+ */
+package Pattern_Programs;
+public class Pattern9 {
+    public static void main(String[] args) {
+        int n=15;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if( i+j <= (n-1)/2 || j-i>=(n-1)/2 || i==0 || i==n-1 || j==0 || j==n-1) {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
