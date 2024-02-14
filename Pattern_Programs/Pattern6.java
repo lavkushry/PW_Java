5a59112f810fe53683f80f12d29965a60d71e87c
lavkushry <lavkushry@gmail.com>
2024-02-14 21:55:42 +0530

    Diagonal Hollow Square Pattern

---
+++ Pattern_Programs/Pattern6.java
@@ -0,0 +1,31 @@
+package Pattern_Programs;
+/*
+Diagonal Hollow Square Pattern
+ * * * * * * * * * *
+ * *             * *
+ *   *         *   *
+ *     *     *     *
+ *       * *       *
+ *       * *       *
+ *     *     *     *
+ *   *         *   *
+ * *             * *
+ * * * * * * * * * *
+
+ * */
+public class Pattern6 {
+    public static void main(String[] args) {
+        int n=10;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i==j || i==0 || j==0 || i == n-1 ||j == n-1 || i+j==n-1){
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
