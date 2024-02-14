9a2c8eac1e673cdecbc7cc5de03bc4165436ec14
lavkushry <lavkushry@gmail.com>
2024-02-14 18:36:36 +0530

    Added New Programs

---
+++ Pattern_Programs/Pattern3.java
@@ -0,0 +1,23 @@
+package Pattern_Programs;
+/*
+ *       *
+ * * * * *
+ *       *
+ *       *
+ *       *
+ */
+public class Pattern3 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if ( i == n/2 || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Pattern_Programs/Pattern4.java
@@ -0,0 +1,4 @@
+package Pattern_Programs;
+
+public class Pattern4 {
+}
